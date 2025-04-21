90f4d48bdab6b2ec1763efbdf9fd29ab9bdd7193
AndresRodriguezAgudelo <andres.rodriguez@imaginamos.com>
2025-04-21 10:56:21 -0500

    update: change test on the deploy

--- shared/components/BotoneraPerfil/index.tsx
+++ shared/components/BotoneraPerfil/index.tsx
@@ -24,7 +24,7 @@ export default function BotoneraPerfil() {
           Perfil
         </Text>
         <Text color="black" styleName="Caption Standard">
-          Santiago Lopera
+          Andres Rodriguez
         </Text>
       </ProfileInfo>
     </Container>
